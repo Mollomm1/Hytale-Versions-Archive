58aa02ca5436f98c4e155dfce50a9086e5f11836
Mollomm1 <68751598+Mollomm1@users.noreply.github.com>
2026-01-19 09:37:42 +0100

    fix linux launch script for future releases

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,10 +3,10 @@
 # Ensure we are in the directory of the script
 cd "$(dirname "$0")"
 
-./data/Client/HytaleClient \
-  --app-dir "$PWD/data" \
+./game/data/Client/HytaleClient \
+  --app-dir "$PWD/game/data" \
   --user-dir "$PWD/UserData" \
-  --java-exec "$PWD/jre/bin/java" \
+  --java-exec "$PWD/game/jre/bin/java" \
   --auth-mode offline \
   --uuid 13371337-1337-1337-1337-133713371337 \
   --name "$USER"
